2946188f624aa90b809d8a8bb1b3186bfd67e509
masonxotto <masonxotto@gmail.com>
2022-10-10 15:47:44 -0500

    started "mock" validation checking of username and password

--- SignIn.js
+++ SignIn.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Dimensions, Pressable } from 'react-native';
-
+import React, {useState} from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Dimensions, Pressable, Alert } from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 
+
 const SignIn = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  //Temporary -- will eventually validate with usernames and passwords from database server.
+  const validateSignIn = () => {
+    if (username != "admin") {
+      setUsername("");
+      setPassword("");
+      return console.log("Invalid Username");
+    }
+    else if (password != "password") {
+      setPassword("");
+      return console.log("Invalid Password");
+    }
+    else {
+      //go to home page
+      setUsername("");
+      setPassword("");
+      return console.log("Success!");
+    }
+  }
+
     return(
       <View style={styles.containerOuter}>
         <Text style={styles.head}>Sign In</Text>
         <View style={styles.containerInner}>
           <Text style={styles.head2}>Username</Text>
-          <TextInput style={styles.userInput} placeholder=""></TextInput>
+            <TextInput value={username} onChangeText={(userInput) => setUsername(userInput)} style={styles.userInput} placeholder=""></TextInput>
           <Text style={styles.head2}>Password</Text>
-          <TextInput secureTextEntry={true} style={styles.userInput} placeholder=""></TextInput>
+            <TextInput value={password} onChangeText={(userInput) => setPassword(userInput)} secureTextEntry={true} style={styles.userInput} placeholder=""></TextInput>
           <Pressable style={styles.forgotPassword}>
             <Text>Forgot password</Text>
           </Pressable>
-          <Button color="#4257f5" title="Login"></Button>
+          <TouchableOpacity style={styles.loginContainer} onPress={() => validateSignIn()}>
+            <Text style={styles.login}>Login</Text>
+          </TouchableOpacity>
         </View>
       </View>
     )
@@ -70,7 +95,20 @@ forgotPassword: {
   justifyContent: 'left',
   width: '500px',
   marginLeft: '2%'
-},  
+}, 
+loginContainer: {
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#4257f5',
+  width: '100px',
+  height: '50px',
+  borderRadius: '10px',
+},
+login: {
+  color: '#fff',
+  fontSize: '18px',
+  fontWeight: 'bold',
+},
 
 });
 
