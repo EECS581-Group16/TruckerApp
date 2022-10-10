3bb779a033ec1481b90df4f04446ac35c4825521
masonxotto <masonxotto@gmail.com>
2022-10-10 17:43:18 -0500

    added .js for HomeScreen

---
+++ HomeScreen.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Dimensions, Pressable, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+
+    return (
+        <View style={styles.container}>
+            <Text>This is the HomeScreen</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default HomeScreen;
