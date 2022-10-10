1c73085b2d85c85ebddb44ced06e45bd86b65a81
masonxotto <masonxotto@gmail.com>
2022-10-10 17:44:04 -0500

    added navigation to HomeScreen after successful login

--- SignIn.js
+++ SignIn.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, TextInput, View, TouchableOpacity, Dimensions, Pressable, Button } from 'react-native';
-const windowWidth = Dimensions.get('window').width;
 
-
-const SignIn = () => {
+const SignIn = ({ navigation }) => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -28,14 +26,14 @@ const SignIn = () => {
     else {
       //go to home page
       console.log("Success!");
+      navigation.navigate('HomeScreen');
       setUsername("");
       setPassword("");
+
     }
   }
-
-  
-
-    return(
+  return(
+    <View style={styles.container}>
       <View style={styles.containerOuter}>
         <Text style={styles.head}>Sign In</Text>
         <View style={styles.containerInner}>
@@ -50,74 +48,81 @@ const SignIn = () => {
             <Text style={styles.login}>Login</Text>
           </TouchableOpacity>
         </View>
-      </View>
-    )
+      </View>  
+    </View>
+  )
+    
   }
 
 const styles = StyleSheet.create({
-containerOuter: {
+  container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'left',
-    justifyContent: 'left',
-    margin: '10%',
-    width: 'windowWidth',
-},
-
-containerInner: {
-    flex: 1,
-    backgroundColor: '#fff',
-    border: 'solid',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '600px',
-    maxHeight: '350px',
+  },
+  containerOuter: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'left',
+      justifyContent: 'left',
+      margin: '10%',
+      width: 'windowWidth',
+  },
+
+  containerInner: {
+      flex: 1,
+      backgroundColor: '#fff',
+      border: 'solid',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '600px',
+      maxHeight: '350px',
+      borderRadius: '10px',
+  },
+
+  userInput: {
+    border: 'solid',
+    padding: '4px',
+    width: '500px',
+    height: '50px',
     borderRadius: '10px',
-},
+    margin: '10px',
+  },
 
-userInput: {
-  border: 'solid',
-  padding: '4px',
-  width: '500px',
-  height: '50px',
-  borderRadius: '10px',
-  margin: '10px',
-},
+  head: {
+    fontSize: '32px',
+    padding: '4px',
+  },
 
-head: {
-  fontSize: '32px',
-  padding: '4px',
-},
+  head2: {
+    fontSize: '24px',
+    alignItems: 'left',
+    justifyContent: 'left',
+    width: '500px',
+    color: '#4257f5',
+    fontWeight: 'bold'
+  },
 
-head2: {
-  fontSize: '24px',
-  alignItems: 'left',
-  justifyContent: 'left',
-  width: '500px',
-  color: '#4257f5',
-  fontWeight: 'bold'
-},
-
-forgotPassword: {
-  alignItems: 'left',
-  justifyContent: 'left',
-  width: '500px',
-  marginLeft: '2%'
-}, 
-loginContainer: {
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundColor: '#4257f5',
-  width: '100px',
-  height: '50px',
-  borderRadius: '10px',
-},
-login: {
-  color: '#fff',
-  fontSize: '18px',
-  fontWeight: 'bold',
-},
-
+  forgotPassword: {
+    alignItems: 'left',
+    justifyContent: 'left',
+    width: '500px',
+    marginLeft: '2%'
+  }, 
+  loginContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4257f5',
+    width: '100px',
+    height: '50px',
+    borderRadius: '10px',
+  },
+  login: {
+    color: '#fff',
+    fontSize: '18px',
+    fontWeight: 'bold',
+  },
 });
 
 
