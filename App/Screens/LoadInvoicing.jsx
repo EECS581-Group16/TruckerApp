118372839b044d2287859b6051d9628a392fa0aa
masonxotto <masonxotto@gmail.com>
2022-10-23 19:18:21 -0500

    added empty pages for Accouting and LoadInvoicing page

---
+++ App/Screens/LoadInvoicing.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Dimensions, Pressable, Button } from 'react-native';
+import { styles, colors } from '../Styles/GlobalStyles.jsx';
+
+/*-------------------------------------------------------------------------
+  Home Screen Component
+-------------------------------------------------------------------------*/
+export default function LoadInvoicing({ navigation }){
+
+  /*-------------------------------------------------------------------------
+    Home Screen
+  -------------------------------------------------------------------------*/
+  return (
+      <View style={styles.pageContainer}>
+        
+      </View>
+  );
+
+}
+
+/*-------------------------------------------------------------------------
+  Component Specific Styles
+-------------------------------------------------------------------------*/
+const homeStyles = StyleSheet.create({
+  mySoftRectangularContainer: {// Soft Rectangular Container
+    backgroundColor: colors.white,
+    border: 'solid',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '600px',
+    height: '350px',
+    borderRadius: '10px',
+  },
+});
