18c43207513a5b0e741582e716124a147b50a9cd
masonxotto <masonxotto@gmail.com>
2022-10-25 17:50:08 -0500

    added switching to reset password component on valid username submission

--- App/Screens/ForgotPassword.jsx
+++ App/Screens/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Text, StyleSheet, TextInput, View, TouchableOpacity, Pressable } from 'react-native';
 import { styles, colors } from '../Styles/GlobalStyles.jsx';
+console.reportErrorsAsExceptions = false;
 
 /*-------------------------------------------------------------------------
   Forgot Password Component
@@ -9,10 +10,21 @@ export default function ForgotPassword({ navigation }){
 /*TODO: write simple function to check for a valid usernamethen display a different 
         UI for validation (probably via email), then display reset password UI
 */
+
+const [validatedUsername, setValidatedUsername] = useState(false);
+
+const ValidatingUsername = () => {
+
   const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("");
 
   const validUsername = () => {
-    
+    if (username === "admin") { //this will eventually run against all username in database to see if there is a match
+      setValidatedUsername(true);
+    }
+    else {
+      setUsernameError("Not Valid Username");
+    }
   }
 
   const handleKeyDown = (e) => {
@@ -22,6 +34,58 @@ export default function ForgotPassword({ navigation }){
   }
 
 
+  return (
+    <View style={signInStyles.mySoftRectangularContainer}>
+      {usernameError.length > 0 && <Text style={styles.errorText}>{usernameError}</Text>}
+      <Text style={[styles.primaryHeader, {justifyContent: "left"}]}>Username *</Text>
+      <TextInput value={username} onChangeText={(textInputBox) => setUsername(textInputBox)} style={styles.textInputBox} placeholder="" onKeyPress={handleKeyDown}></TextInput>
+      <TouchableOpacity style={styles.button} onPress={() => validUsername()}>
+      <Text style={styles.loginText}>Forgot Password</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const ResetPassword = () => {
+  
+  const [newPassword, setNewPassword] = useState("");
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
+  const [validatedPassword, setValidatedPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+
+  const validPassword = () => {
+    if (newPassword === newPasswordConfirm) { //this will eventually run against pararameters for a password, such as 8 characters in length, number, etc. it will also ensure it is not the old password
+      setValidatedPassword(true);
+      if (passwordError.length > 0) {
+        setPasswordError("");
+      }
+    }
+    else {
+      setPasswordError("Passwords do not match!");
+    }
+  }
+
+  const handleKeyDown = (e) => {
+    if(e.nativeEvent.key == "Enter"){
+      validPassword();
+    }
+  }
+
+
+  return (
+    <View style={signInStyles.mySoftRectangularContainer}>
+        {passwordError.length > 0 && <Text style={styles.errorText}>{passwordError}</Text>}
+        <Text style={[styles.primaryHeader, {justifyContent: "left"}]}>New Password *</Text>
+        <TextInput value={newPassword} onChangeText={(textInputBox) => setNewPassword(textInputBox)} style={styles.textInputBox} secureTextEntry={true} placeholder="" onKeyPress={handleKeyDown}></TextInput>
+        <Text style={[styles.primaryHeader, {justifyContent: "left"}]}> Confirm New Password *</Text>
+        <TextInput value={newPasswordConfirm} onChangeText={(textInputBox) => setNewPasswordConfirm(textInputBox)} style={styles.textInputBox} secureTextEntry={true} placeholder="" onKeyPress={handleKeyDown}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={() => validPassword()}>
+        <Text style={styles.loginText}>Reset Password</Text>
+        </TouchableOpacity>
+      </View>
+  );
+}
+
   /*-------------------------------------------------------------------------
     Forgot Password Screen
   -------------------------------------------------------------------------*/
@@ -29,13 +93,8 @@ export default function ForgotPassword({ navigation }){
   return(
     <View style={styles.pageContainer}>
         <Text style={styles.pageTitle}>Forgot Password</Text>
-        <View style={signInStyles.mySoftRectangularContainer}>
-        <Text style={[styles.primaryHeader, {justifyContent: "left"}]}>Username *</Text>
-          <TextInput value={username} onChangeText={(textInputBox) => setUsername(textInputBox)} style={styles.textInputBox} placeholder="" onKeyPress={handleKeyDown}></TextInput>
-          <TouchableOpacity style={styles.button} onPress={() => validUsername()}>
-          <Text style={styles.loginText}>Forgot Password</Text>
-        </TouchableOpacity>
-        </View>
+        {validatedUsername === false && <ValidatingUsername />}
+        {validatedUsername === true && <ResetPassword />}
     </View>
   )
     
