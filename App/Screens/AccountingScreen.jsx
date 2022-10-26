2f56fb542ab553d75e23c3046ba5eed74b222de2
Ryan Penrod <ryanmpenrod@gmail.com>
2022-10-26 02:19:34 -0500

    Added and created outline for accounting page

--- App/Screens/AccountingScreen.jsx
+++ App/Screens/AccountingScreen.jsx
@@ -13,8 +13,95 @@ export default function AccountingScreen(props){
   -------------------------------------------------------------------------*/
   return (
     <View style={globalStyles.pageContainer}>
-      Hello
+      <View style={styles.outerContainer}>
+        <Text style={styles.title}>
+          Accounting
+        </Text>
+        <View style={styles.softContainer}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.truckNum}>
+              Truck #:
+            </Text>
+            <Text style={styles.driver}>
+              Driver:
+            </Text>
+          </View>
+          <View style={styles.cardBody}>
+            <Text>
+              Date:
+            </Text>
+            <Text>
+              Ticket #:
+            </Text>
+            <Text>
+              Order #:
+            </Text>
+            <Text>
+              Hours:
+            </Text>
+            <Text>
+              Tons:
+            </Text>
+            <Text>
+              Unit Price:
+            </Text>
+            <Text>
+              Total:
+            </Text>
+          </View>
+          <View style={styles.cardFooter}>
+            <Text style={styles.truckTotal}>
+              Truck Total:
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 
 }
+
+const styles = StyleSheet.create({
+  cardHeader : {
+    width: "90%",
+    textAlign: "left",
+  },
+  cardFooter : {
+    width: "90%",
+    textAlign: "right",
+  },
+  outerContainer : {
+    width: "80%",
+    height: "80%",
+  },
+  softContainer : {
+    backgroundColor: colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+    border: "solid",
+    borderRadius: "10px",
+    width: "100%"
+  },
+  cardBody : {
+    backgroundColor: colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+    border: "solid",
+    borderRadius: "10px",
+    width: "90%"
+  },
+  title : {
+    textAlign: "left",
+    fontSize: 50,
+    paddingBottom: 15,
+  },
+  truckNum : {
+    fontSize: 20,
+  },
+  truckTotal : {
+    fontSize: 20,
+  },
+  driver : {
+    fontSize: 20,
+  }
+});
