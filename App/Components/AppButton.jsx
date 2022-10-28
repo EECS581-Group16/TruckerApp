4709f11b31715b17d30006e57370e02d758b60e2
masonxotto <masonxotto@gmail.com>
2022-10-28 12:51:50 -0500

    updated AppButton width to 50%

--- App/Components/AppButton.jsx
+++ App/Components/AppButton.jsx
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     button: {
         backgroundColor: colors.primary,
         flex: 1,
-        width: "100%",
+        width: "50%",
         height: "100%",
         justifyContent: "center",
         alignItems: "center",
